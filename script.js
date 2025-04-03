a2191419fb9264577c1aeb813a8360103dadb8b4
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2025-04-03 12:17:21 -0500

    made themes

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("your mother")
+
+// Function to toggle theme
+document.querySelector(".theme-button").addEventListener("click", function() {
+    document.querySelector(".theme-menu").classList.toggle("show");
+  });
+
+  function setTheme(theme) {
+    localStorage.setItem("theme", theme);
+    document.body.setAttribute('data-theme', theme);
+  }
+
+  // Apply the theme based on localStorage or system preference
+  const savedTheme = localStorage.getItem('theme') || 'auto';
+  document.body.setAttribute('data-theme', savedTheme);
