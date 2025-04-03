79b0cde446d032e4378bda85ebc4b3c0e39a8b7a
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2025-04-03 12:30:11 -0500

    seperate js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// Function to toggle theme menu
+console.log("your_mother")
+
+// Function to toggle the theme menu
 document.querySelector(".theme-button").addEventListener("click", function() {
     document.querySelector(".theme-menu").classList.toggle("show");
   });
-
+  
+  // Function to set the theme based on user selection
   function setTheme(theme) {
     localStorage.setItem("theme", theme);
     document.body.setAttribute('data-theme', theme);
   }
-
+  
   // Apply the theme based on localStorage or system preference
   const savedTheme = localStorage.getItem('theme') || 'auto';
   document.body.setAttribute('data-theme', savedTheme);
+  
