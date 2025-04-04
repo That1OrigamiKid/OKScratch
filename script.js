956bec2517ab940d6ea551494fd66a2f2ce06d9b
That1OrigamiKid <132720108+That1OrigamiKid@users.noreply.github.com>
2025-04-04 11:48:49 -0500

    added base code for cave jumper

--- script.js
+++ script.js
@@ -66,4 +66,16 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
-photos/profile-dark.png
+document.getElementById("fullscreen-btn").addEventListener("click", function() {
+  const gameFrame = document.getElementById("game-frame");
+
+  if (gameFrame.requestFullscreen) {
+      gameFrame.requestFullscreen();
+  } else if (gameFrame.mozRequestFullScreen) { // Firefox
+      gameFrame.mozRequestFullScreen();
+  } else if (gameFrame.webkitRequestFullscreen) { // Chrome, Safari, Opera
+      gameFrame.webkitRequestFullscreen();
+  } else if (gameFrame.msRequestFullscreen) { // IE/Edge
+      gameFrame.msRequestFullscreen();
+  }
+});
